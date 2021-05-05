a9677aa6dd55b35d47ef3f1a71735501569592f8
ASAngelica <asa196549@gmail.com>
2021-05-05 19:44:06 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,13 +1,25 @@
 package com.devsuperior.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	@Query("Select new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller") // linguagem JPQL
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("Select new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller") // linguagem JPQL
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
 
 // Só com isso nós já temos um objeto, SellerRepository, responsável por acessar os dados de um Seller
 // vamos ter as operações de inserir, buscar, deletar, atualizar tudo isso já vem implementado por padrão
-// pelo framework.
+// pelo framework. Também a consulta agrupada da soma por vendedor.
